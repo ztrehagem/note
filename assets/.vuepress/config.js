1ee369fe8f3e76f40dc23d73db408588bc9ee5ce
Shigeki Suwa <shigeki.suwa@ztrehagem.dev>
2020-03-29 15:42:33 +0900

    vuepress

---
+++ assets/.vuepress/config.js
@@ -0,0 +1,30 @@
+const title = 'note.ztrehagem.dev'
+const description = 'Just notes.'
+const publicPath = 'https://note.ztrehagem.dev/'
+
+module.exports = {
+  title,
+  description,
+
+  head: [
+    ['link', { rel: 'icon', type: 'image/x-icon', href: '/favicon.ico' }],
+    ['meta', { property: 'og:title', content: title }],
+    ['meta', { property: 'og:type', content: 'website' }],
+    ['meta', { property: 'og:url', content: publicPath }],
+    ['meta', { property: 'og:image', content: 'https://github.com/ztrehagem.png' }],
+    ['meta', { property: 'og:locale', content: 'ja_JP' }],
+    ['meta', { property: 'og:site_name', content: title }],
+    ['meta', { property: 'og:description', content: description }],
+    ['meta', { name: 'twitter:card', content: 'summary' }],
+    ['meta', { name: 'twitter:site', content: '@ztrehagem' }],
+    ['meta', { name: 'theme-color', content: '#3C3C3C' }],
+    ['meta', { name: 'msapplication-navbutton-color', content: '#3C3C3C' }],
+    ['meta', { name: 'apple-mobile-web-app-status-bar-style', content: '#3C3C3C' }],
+  ],
+
+  locales: {
+    '/': {
+      lang: 'ja_JP',
+    },
+  },
+}
